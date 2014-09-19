228d9f4052ac2eee39e50b16540633b132095e3b
Dan Siegel <dsiegel@cari.net>
2014-09-19 08:23:10 -0700

    Added model templates for FAQ #IBPLIB-13 #start #IBPLIB-13 #finish #IBPLIB-13 #deliver #IBPLIB-13 #accept #IBPLIB-13 #publish

---
+++ iBizProduct/Models/Templates/FAQBase.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) iBizVision - 2014
+// Author: Dan Siegel
+
+using System.ComponentModel.DataAnnotations.Schema;
+using iBizProduct.DataContracts;
+
+namespace iBizProduct.Models.Templates
+{
+    /// <summary>
+    /// This provides a common framework to work with the Panel's FAQ featureset
+    /// </summary>
+    /// <typeparam name="T">Id type of the Keys</typeparam>
+    public abstract class FAQBase<T>
+    {
+        /// <summary>
+        /// FAQ Database Id
+        /// </summary>
+        [DatabaseGenerated( DatabaseGeneratedOption.Identity )]
+        public T FAQId { get; set; }
+
+        /// <summary>
+        /// FAQ ElementLanguage
+        /// </summary>
+        public Language DefaultLanguage { get; set; }
+    }
+}
---
+++ iBizProduct/Models/Templates/FAQMessageBase.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) iBizVision - 2014
+// Author: Dan Siegel
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using iBizProduct.DataContracts;
+
+namespace iBizProduct.Models.Templates
+{
+    /// <summary>
+    /// Provides a context for storing information on a FAQ Item in multiple languages.
+    /// </summary>
+    /// <typeparam name="T">Id Type ( int or Guid )</typeparam>
+    public abstract class FAQMessageBase<T>
+    {
+        /// <summary>
+        /// FAQMessage Id
+        /// </summary>
+        [DatabaseGenerated( DatabaseGeneratedOption.Identity )]
+        public T FAQMessageId { get; set; }
+
+        /// <summary>
+        /// FAQ Database Id
+        /// </summary>
+        public T FAQId { get; set; }
+
+        /// <summary>
+        /// FAQ ElementLanguage
+        /// </summary>
+        public Language Language { get; set; }
+
+        /// <summary>
+        /// FAQ Title
+        /// </summary>
+        [MaxLength( 255 )]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// FAQ Content
+        /// </summary>
+        public string Content { get; set; }
+    }
+}
