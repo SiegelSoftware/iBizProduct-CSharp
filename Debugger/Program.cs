0eb8b6c244b523540100cafcb26b7d3c614c5456
Dan Siegel <dsiegel@cari.net>
2014-09-22 14:55:09 -0700

    Added Debugger Console Project to solution.

---
+++ Debugger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iBizProduct;
+using iBizProduct.DataContracts;
+
+namespace Debugger
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+
+        }
+    }
+}
