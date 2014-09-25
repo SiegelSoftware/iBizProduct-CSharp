2a8015bf245cb4d4d6d490669b443b6974cae57b
William Cahill-Manley <wmanley@cari.net>
2014-09-25 16:14:37 -0700

    Generic Hasher methods, sha 256 and 512

---
+++ iBizProduct/Security/GenericHasher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iBizProduct.Security
+{
+    class GenericHasher : IHasherInterface
+    {
+        private HashAlgorithm Manager { get; set; }
+
+        public GenericHasher(HashAlgorithm manager)
+        {
+            this.Manager = manager;
+        }
+
+        public byte[] Hash ( byte[] toHash )
+        {
+            return this.Manager.ComputeHash( toHash );
+        }
+
+        public string Hash ( string toHash )
+        {
+            byte[] hash = this.Hash( Encoding.UTF8.GetBytes( toHash ) );
+
+            string hexHash = "";
+
+            foreach(byte x in hash)
+            {
+                hexHash += String.Format( "{0:x2}", x );
+            }
+
+            return hexHash;
+        }
+    }
+}
---
+++ iBizProduct/Security/IHasherInterface.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) iBizVision - 2014
+// Author: William Cahill-Manley
+
+namespace iBizProduct.Security
+{
+    public interface IHasherInterface
+    {
+        string Hash( string toHash );
+        byte[] Hash( byte[] toHash );
+
+    }
+}
